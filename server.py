b606974b7d95c8f6cd8cbb7cd1aa7fda51393846
natukimiyabi23-eng <natuki.miyabi23@gmail.com>
2026-07-09 03:53:24 +0900

    Fix Render bot startup

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import os
 import threading
-import subprocess
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,7 +11,7 @@ def home():
 
 
 def start_bot():
-    subprocess.Popen(["python", "x_bot.py"])
+    import x_bot
 
 
 threading.Thread(target=start_bot, daemon=True).start()

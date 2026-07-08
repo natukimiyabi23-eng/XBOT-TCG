80fa42544f8181f82fc33bd664ea49a470ff5014
natukimiyabi23-eng <natuki.miyabi23@gmail.com>
2026-07-09 01:18:07 +0900

    Step 2: RSS account loading completed

--- x_bot.py
+++ x_bot.py
@@ -1,6 +1,49 @@
 import discord
+import feedparser
 from config import TOKEN, CHANNEL_ID
 
+def load_accounts():
+    accounts = []
+
+    with open("accounts.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+
+            if not line:
+                continue
+
+            name, rss_url = line.split("|")
+
+            accounts.append({
+                "name": name,
+                "rss_url": rss_url
+            })
+
+    return accounts
+    
+
+accounts = load_accounts()
+
+print("===== RSS一覧 =====")
+
+for account in accounts:
+
+    print()
+    print("================================")
+    print("名前:", account["name"])
+    print("RSS:", account["rss_url"])
+
+    feed = feedparser.parse(account["rss_url"])
+
+    latest_post = feed.entries[0]
+
+    print("最新記事")
+
+    print("タイトル:", latest_post.title)
+
+    print("リンク:", latest_post.link)
+
+    print("===================")
 client = discord.Client(intents=discord.Intents.default())
 
 
